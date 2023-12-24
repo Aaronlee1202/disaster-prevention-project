f07805a89d39923d92ce6eaa77085f4777df945e
AaronLee <aaronlee120213@gmail.com>
2023-12-24 16:04:32 +0800

    修改路徑

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+import '@/css/bootstrap.min.css';
+import '@/css/theme.css';
+import '@/css/theme-elements.css';
+import '@/scss/style.scss';
+
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
