6408760756032db56cb200056e63cbda8382126d
Aaron <aaronlee120213@gmail.com>
2024-02-22 16:57:35 +0800

    修改router base url, 修改資料夾名稱

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import PrimarySchoolView from '../views/PrimarySchool/PrimarySchoolView.vue';
-import DisasterPreventionView from '../views/January/DisasterPreventionView.vue';
-import FebruaryView from '@/views/February/FebruaryView.vue';
+import DisasterPreventionView from '../views/Jan/DisasterPreventionView.vue';
+import FebruaryView from '@/views/Feb/FebruaryView.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('NCDR-Topic/2024'),
   routes: [
     {
       path: '/',
