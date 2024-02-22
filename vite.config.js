9cf011522530eb47180cdb0330fe708b8de21d30
Aaron <aaronlee120213@gmail.com>
2024-02-22 17:05:08 +0800

    update base url

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/disaster-prevention-project/NCDR-Topic/2024' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/disaster-prevention-project/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
