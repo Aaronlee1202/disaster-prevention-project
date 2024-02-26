b2940f91205236f85a5e0fd2036c75b07e47bcfd
Aaron <aaronlee120213@gmail.com>
2024-02-26 16:22:37 +0800

    update router

--- src/router/index.js
+++ src/router/index.js
@@ -7,18 +7,19 @@ var u = navigator.userAgent;
 // ua = navigator.userAgent.toLowerCase();
 let isLineApp = u.indexOf('Line') > -1 ? true : false; //Line 內建瀏覽器
 let isFbApp = u.indexOf('FBAV') > -1 ? true : false; // FB App 內建瀏覽器
+console.log('isLineApp', isLineApp, 'isFbApp', isFbApp);
 
 const base_url = import.meta.env.BASE_URL;
-let url;
+// let url;
 
-if (isLineApp || isFbApp) {
-  url = base_url + '?openExternalBrowser=1';
-} else {
-  url = base_url;
-}
+// if (isLineApp || isFbApp) {
+//   url = base_url + '?openExternalBrowser=1';
+// } else {
+//   url = base_url;
+// }
 
 const router = createRouter({
-  history: createWebHistory(url),
+  history: createWebHistory(base_url),
   routes: [
     {
       path: '/',
