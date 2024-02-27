a8e16d7d45ac2c4ca7a25f5d22fc895fe385098b
Aaron <aaronlee120213@gmail.com>
2024-02-27 16:54:44 +0800

    在根目錄新增檢查內建瀏覽器 測試2

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,27 @@
 <script setup>
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import { RouterView } from 'vue-router';
 import Header from './components/NavHeader.vue';
 import Footer from './components/NavFooter.vue';
 import BackToTop from './components/BackToTop.vue';
 
-var userAgent = navigator.userAgent.toUpperCase();
-let isLineApp = userAgent.indexOf('Line') > -1 ? true : false; //Line 內建瀏覽器
-let isFbApp = userAgent.indexOf('FBAV') > -1 ? true : false; // FB App 內建瀏覽器
+// const router = useRouter();
+let isLineApp = ref(false); //Line 內建瀏覽器
+let isFbApp = ref(false); // FB App 內建瀏覽器
+const settimeout = ref(false);
+
 onMounted(() => {
+  var userAgent = navigator.userAgent.toUpperCase();
+  isLineApp.value = userAgent.indexOf('Line') > -1 ? true : false; //Line 內建瀏覽器
+  isFbApp.value = userAgent.indexOf('FBAV') > -1 ? true : false; // FB App 內建瀏覽器
   setTimeout(() => {
+    settimeout.value = true;
     isApp();
-  }, 2000);
+  }, 3000);
 });
 
 function isApp() {
+  console.log(settimeout.value);
   if (isLineApp || isFbApp) {
     const location = window.location.href;
     window.open(`${location}?openExternalBrowser=1`);
@@ -24,6 +31,7 @@ function isApp() {
 
 <template>
   <Header />
+  <div>{{ isLineApp }}{{ isFbApp }}</div>
   <RouterView />
   <BackToTop />
   <Footer />
